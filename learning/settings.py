511a7a25e739d0be15e5c43744d00014463df2e7
Milena Kowalska <milenakowalska126@gmail.com>
2020-10-15 00:05:58 +0200

    Commit

--- learning/settings.py
+++ learning/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '09_=vf#_1700p0p^)z27(%+h%4wue-^wn-ki6np+yeaau5tg*n'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
